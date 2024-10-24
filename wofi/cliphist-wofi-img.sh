0d5e92799884dbeac4b5cc3c89106486ddcdcd9e
warpje5 <125624948+warpje5@users.noreply.github.com>
2024-10-24 17:13:13 +0300

    Add files via upload

--- wofi/cliphist-wofi-img.sh
+++ wofi/cliphist-wofi-img.sh
@@ -17,7 +17,7 @@ cliphist_list="$(cliphist list)"
 for thumb in "$thumb_dir"/*; do
     clip_id="${thumb##*/}"
     clip_id="${clip_id%.*}"
-    check=$(rg <<< "$cliphist_list" "^$clip_id\s")
+    check=$(rg <<<"$cliphist_list" "^$clip_id\s")
     if [ -z "$check" ]; then
         >&2 rm -v "$thumb"
     fi
@@ -30,14 +30,14 @@ read -r -d '' prog <<EOF
 /^[0-9]+\s<meta http-equiv=/ { next }
 match(\$0, /^([0-9]+)\s(\[\[\s)?binary.*(jpg|jpeg|png|bmp)/, grp) {
     image = grp[1]"."grp[3]
-    system("[ -f $thumb_dir/"image" ] || echo " grp[1] "\\\\\t | cliphist decode | convert - -resize '256x256>' $thumb_dir/"image )
+    system("[ -f $thumb_dir/"image" ] || echo " grp[1] "\\\\\t | cliphist decode | convert - -resize '288x288>' $thumb_dir/"image )
     print "img:$thumb_dir/"image
     next
 }
 1
 EOF
 
-choice=$(gawk <<< $cliphist_list "$prog" | wofi -I --dmenu)
+choice=$(gawk <<<$cliphist_list "$prog" | wofi -I --dmenu)
 
 # stop execution if nothing selected in wofi menu
 [ -z "$choice" ] && exit 1
